b6a13b918824c19e10d0475adb039cfc0743f67c
ralfralf123 <rjganastacio@mymail.mapua.edu.ph>
2023-07-11 16:14:35 +0800

    Upload files

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 31337 // Replace with any chain ID you prefer for your local network
+    }
+  },
+  solidity: "0.8.0" 
+};
